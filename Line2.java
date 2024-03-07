2365efbc2f8804ddaad1701f7b6d42d5455c57aa
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2024-03-07 18:32:45 +0100

    Create Line2.java

---
+++ Line2.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package readLine;
+
+/**
+ *
+ * @author ariadna
+ */
+public class Line {
+
+    protected StringBuilder text;
+    protected static int posCursor;
+    protected boolean modeins;
+    protected String inici;
+    protected String fi;
+
+
+    public Line() {
+        text = new StringBuilder();
+        inici = new String();
+        fi = new String();
+        posCursor = 0;
+        modeins = false;
+
+    }
+
+    public void add(char c) {
+        
+        if(posCursor == text.length()){
+            text.append(c);
+        }else{
+            fi = text.substring(posCursor);
+            text.setLength(posCursor);
+            text.append(c);
+            text.append(fi);
+            
+        }
+        posCursor++;
+
+    }
+
+    public void delete() {
+        text.deleteCharAt(posCursor);
+        posCursor--;
+    }
+
+    public boolean moveCursorLeft() {
+        if (posCursor == 0){
+            return false;
+        }else {
+            posCursor--;
+            return true;
+        }
+    }
+
+    public boolean moveCursorRight() {
+        
+        if (posCursor == text.length()){
+            return false;
+        }else {
+            posCursor++;
+            return true;
+        }
+
+    }
+
+    public void moveCursorHome() {
+        posCursor = 0;
+
+    }
+
+public void moveCursorFin() {
+        posCursor = text.length();
+    }
+
+    public void ins() { // CONMUTA SOBRE INSERTAR/SOBREESCRIURE
+        modeins = !modeins;
+    }
+
+    public String getText() {
+        return text.toString();
+    }
+
+    public int getCursorPosition() {
+        return posCursor;
+    }
+
+}
