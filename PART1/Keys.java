e0c28ed48725016e5325407fda5567aa831d0813
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2024-03-13 09:50:16 +0100

    Update Keys.java

--- PART1/Keys.java
+++ PART1/Keys.java
@@ -1,20 +1,24 @@
-package PART1;
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package readLine;
 
 /**
  *
+ * @author ariadna
  */
 public class Keys {
 
     protected static final String ANSI_ESC = "\u001B[";
     protected static final String ANSI_RIGHT = ANSI_ESC + "C";
     protected static final String ANSI_LEFT = ANSI_ESC + "D";
-    protected static final String ANSI_HOME = ANSI_ESC + "H";
+    protected static final String ANSI_HOME = ANSI_ESC + "1G";
     protected static final String ANSI_END = ANSI_ESC + "F";
-    protected static final String ANSI_INS = ANSI_ESC + "[2~";
+    //protected static final String ANSI_INS = ANSI_ESC + "[2~";
     protected static final String ANSI_RET = "\r";
-    protected static final String ANSI_BCKS = "\b";
-    protected static final String ANSI_DEL = ANSI_ESC + "[3~";
+    protected static final String ANSI_DEL = "\b \b";
+    protected static final String ANSI_DEL_LINE = ANSI_ESC + "[2K\r";
     
     protected static final int ESCAPE = 27;
     protected static final int RIGHT = 67;
