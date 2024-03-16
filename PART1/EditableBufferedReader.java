cbb94f187e72c3909adae60621a1c53a02d6d002
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2024-03-16 20:42:08 +0100

    Update EditableBufferedReader.java

--- PART1/EditableBufferedReader.java
+++ PART1/EditableBufferedReader.java
@@ -1,116 +1,246 @@
-package PART1;
-import java.io.*;
+package readLine;
 
+import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+parse:
+HOME: ESC O H 
+END: ESC O F
+RIGHT: ESC [ C
+LEFT: ESC [ D
+HOME: ESC [ 1 ~
+INS: ESC [ 2 ~
+DEL: ESC [ 3 ~
+END: ESC [ 4 ~
+ */
+
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author ariadna
+ */
 public class EditableBufferedReader extends BufferedReader {
 
-    public EditableBufferedReader(InputStreamReader in) {
+    public EditableBufferedReader(Reader in) {
         super(in);
-        setRaw();
-
     }
 
-    public void setRaw() { // posa el terminal en mode raw
-        String[] cmd = { "/bin/sh", "-c", "stty -echo raw </dev/tty" };
+    /*
+    public static void main(String[] args) {
         try {
-            Runtime.getRuntime().exec(cmd);
+            
+            System.out.println("passem a mode raw");
+            setRaw();
+            Thread.sleep(3000);
+            unsetRaw();
+            System.out.println("Hem tornat a mode cooked");
+             
+
+            BufferedReader in = new EditableBufferedReader(
+                   new InputStreamReader(System.in));
+
+            int c = in.read();
+
+            System.out.println("Caràcter llegit:" + c);
+
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+    }*/
+    public static void setRaw() {
+
+        List<String> command = new ArrayList<>();
+        command.add("/bin/sh");
+        command.add("-c");
+        command.add("stty -echo raw");
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder(command);
+
+            pb.inheritIO();
+
+            Process p = pb.start();
+            p.waitFor(); // Esperem a que el terminal canvi el mode
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public void unsetRaw() { // restaura el terminal al mode normal
-        String[] cmd = { "/bin/sh", "-c", "stty sane </dev/tty" };
-        try {
-            Runtime.getRuntime().exec(cmd);
-        } catch (IOException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 
-    public void clear() { // borra tot de la pantalla (hauria d'anar a line.java)
-        System.out.print("\u001b[2J");
-        System.out.print("\u001b[H");
+    public static void unsetRaw() { // passa de mode RAW a COOKED
+
+        List<String> command = new ArrayList<>();
+        command.add("/bin/sh");
+        command.add("-c");
+        command.add("reset");
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder(command);
+
+            pb.inheritIO();
+
+            Process p = pb.start();
+            p.waitFor(); // Esperem a que el terminal canvi el mode
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public int read() throws IOException {
-        try {
-            int car = super.read(); // no se si he de modificar o no
-            if (car == 27) { // si es esc, triem entre els diferents caracters que poden venir: [home, ]
-                int car2 = super.read(); // el descartem que sera [
-                car2 = super.read(); // es el numero que decideix quin es el caracter
+        /*
+        int nextChar = super.read();
+        if (nextChar == Line.ANSI_ESCAPE) {
 
-                switch (car2) {
-                    case 49: // home (1)
-                        car = -2;
+            nextChar = super.read(); // Leer el siguiente carácter para determinar si es una secuencia de escape
+
+            if (nextChar == '[') { // Si es una secuencia de escape, leer el tercer carácter (el comando de control)
+
+                nextChar = super.read();
+                
+                switch (nextChar) {
+                    case 'C':
+                        return Line.LEFTS;
+                    case 'D':
+                        return Line.CURSOR_FORWARD;
+                    case 'H':
+                        return Line.HOME;
+                    case 'F':
+                        return Line.END;
+                    case '2':
+                        nextChar = super.read();
+                        
+                        return Line.INS;
+                    case '3':
+                        nextChar = super.read();
+                        return Line.DELETE;
+                    default:
+                        nextChar = super.read();
                         break;
-                    case 50: // insert (2)
-                        car = -3;
-                        break;
-                    case 51: // Delete (3)
-                        car = -4;
-                        break;
-                    case 68: // Right
-                        car = -5;
-                        break;
-                    case 67: // Left
-                        car = -6;
-                        break;
+
+                }
+
+            }
+        }
+        
+        if (nextChar == Line.EOT | nextChar == Line.EXIT) {
+            try {
+                this.unsetRaw();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(EditableBufferedReader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.exit(0);
+        }
+        
+        return nextChar;
+         */
+
+        int c = super.read();
+        if (c != Keys.ESCAPE) {
+            return c;
+        }
+        switch (c = super.read()) {
+            case 'O':
+                switch (c = super.read()) {
+                    case 'H':
+                        return Keys.HOME;
+                    case 'F':
+                        return Keys.END;
+                    default:
+                        return c;
+
+                }
+            case '[':
+                switch (c = super.read()) {
+                    case 'D':
+                        return Keys.LEFT;
+                    case 'C':
+                        return Keys.RIGHT;
+                    case '2':
+                        super.read();
+                        return Keys.INS;
+                    case '3':
+                        super.read();
+                        return Keys.DEL;
 
                     default:
-                        break;
+                        return c;
+
                 }
-            }
-            return car;
-        } catch (IOException e) {
-            e.printStackTrace();
+            default:
+                return c;
         }
-        return 0;
+
     }
 
-    public String readLine() throws IOException { // llegeix una lina sencera a partir de read()
+    public String readLine() throws IOException {
         Line line = new Line();
-        try {
+        this.setRaw();
 
-            int res = 0;
-            while (res != 13) {
-                res = read();
-                switch (res) {
-                    case -5: // right
-                        line.Move_cursor_left();
-                        break;
-                    case -6: // left
-                        line.Move_cursor_right();
-                    default: // resta de caracters
-                        line.Add_car((char) res);
-                        break;
-                }
-                clear();
-                line.printline();
+        int r = read();
+        while (r != Keys.RETURN) {
+            switch (r) {
+                case Keys.LEFT:
+                    if (line.moveCursorLeft()) {
+                        System.out.print(Keys.ANSI_LEFT);
+                    }
+                    break;
+                case Keys.END:
+                    line.moveCursorFin();
+                    System.out.print(Keys.ANSI_HOME);
+                    System.out.print(Keys.ANSI_ESC + '[' + line.getCursorPosition() + 'C');
+                    break;
+                case Keys.RIGHT:
+                    if (line.moveCursorRight()) {
+                        System.out.print(Keys.ANSI_RIGHT);
+                    }
+                    break;
+                case Keys.HOME:
+                    line.moveCursorHome();
+                    System.out.print(Keys.ANSI_HOME);
+                    break;
+                case Keys.DEL:
+                    if (line.delete()) {
+                        System.out.print(Keys.ANSI_LEFT);
+                        System.out.print(Keys.ANSI_DEL);
+                    }
+                    break;
+                case Keys.INS:
+                    line.modeins();
+                    break;
+                default:
+                    if (line.getCursorPosition() == line.text.length()) {
+                        line.add((char) r);
+                        System.out.print((char) r);
+                    } else {
+                        
+                        StringBuilder aux = new StringBuilder();
+                        System.out.print(Keys.ANSI_DEL_UNTIL_END);
+                        line.add((char) r);
+                        System.out.print((char) r);
+                        System.out.print(line.fi);
+                        System.out.print("\033" + '[' + (line.getCursorPosition()+1) + "G");
+
+                    }
             }
-            unsetRaw();
-            return line.getlinia();
-        } catch (IOException e) {
-            e.printStackTrace();
+            r = read();
         }
-        return null;
+
+        this.unsetRaw();
+
+        return line.getText();
     }
 
-    // public String readLine() throws IOException { // llegeix una lina sencera a
-    // partir de read()
-    // try {
-    // int res1 = read(); // 27 es ESC
-    // int res2 = read();
-    // int res3 = read();
-    // int res4 = read();
-    // unsetRaw();
-
-    // String seq2 = String.valueOf(res1) + ", " + String.valueOf(res2) + ", " +
-    // String.valueOf(res3) + ", " + String.valueOf(res4); //imprimeix 4 ints (per
-    // les ANSI sequences)
-    // return seq2;
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // return null;
-    // }
 }
