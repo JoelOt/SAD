fe0283b60bec225f4f7d683d87dfede7b09ee163
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2024-03-16 20:43:20 +0100

    Update Keys.java

--- MVC/Keys.java
+++ MVC/Keys.java
@@ -1,29 +1,34 @@
-package MVC;
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PART1;
 
 /**
  *
+ * 
  */
 public class Keys {
 
-    protected static final String ANSI_ESC = "\u001B[";
-    protected static final String ANSI_RIGHT = ANSI_ESC + "C";
-    protected static final String ANSI_LEFT = ANSI_ESC + "D";
-    protected static final String ANSI_HOME = ANSI_ESC + "H";
-    protected static final String ANSI_END = ANSI_ESC + "F";
-    protected static final String ANSI_INS = ANSI_ESC + "[2~";
-    protected static final String ANSI_RET = "\r";
-    protected static final String ANSI_BCKS = "\b";
-    protected static final String ANSI_DEL = ANSI_ESC + "[3~";
+    public static final String ANSI_ESC = "\033";
+    public static final String ANSI_RIGHT = ANSI_ESC + "[C";
+    public static final String ANSI_LEFT = ANSI_ESC + "[D";
+    public static final String ANSI_HOME = ANSI_ESC + "[1G";
+    //public static final String ANSI_END = ANSI_ESC+ [ + <nombre de cols> + C;
+    public static final String ANSI_INS = ANSI_ESC + "[2~";
+    public static final String ANSI_RET = "\r";
+    public static final String ANSI_DEL = ANSI_ESC + "[1P";
+    public static final String ANSI_DEL_UNTIL_END = ANSI_ESC + "[K";
     
-    protected static final int ESCAPE = 27;
-    protected static final int RIGHT = 67;
-    protected static final int LEFT = 68;
-    protected static final int HOME = 72;
-    protected static final int END = 70;
-    protected static final int INS = 155;
-    protected static final int RETURN = 13;
-    protected static final int BCKS = 8;
-    protected static final int DEL = 127;
+    public static final int ESCAPE = 27;
+    public static final int RIGHT = 67;
+    public static final int LEFT = 68;
+    public static final int HOME = 72;
+    public static final int END = 70;
+    public static final int INS = 155;
+    public static final int RETURN = 13;
+    public static final int BCKS = 8;
+    public static final int DEL = 127;
+    
     
 }
