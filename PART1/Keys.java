fb8b698db6ac9d8abaf313d7c71236aa337c8eb6
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2024-03-16 20:40:58 +0100

    Update Keys.java

--- PART1/Keys.java
+++ PART1/Keys.java
@@ -6,28 +6,29 @@ package PART1;
 
 /**
  *
- * @author ariadna
+ * @author 
  */
 public class Keys {
 
-    public static final String ANSI_ESC = "\u001B[";
-    public static final String ANSI_RIGHT = ANSI_ESC + "C";
-    public static final String ANSI_LEFT = ANSI_ESC + "D";
-    public static final String ANSI_HOME = ANSI_ESC + "1G";
-    public static final String ANSI_END = ANSI_ESC + "F";
-    //public static final String ANSI_INS = ANSI_ESC + "[2~";
-    public static final String ANSI_RET = "\r";
-    public static final String ANSI_DEL = "\b \b";
-    public static final String ANSI_DEL_LINE = ANSI_ESC + "[2K\r";
+   
+    protected static final String ANSI_ESC = "\033";
+    protected static final String ANSI_RIGHT = ANSI_ESC + "[C";
+    protected static final String ANSI_LEFT = ANSI_ESC + "[D";
+    protected static final String ANSI_HOME = ANSI_ESC + "[1G";
+    //protected static final String ANSI_END = ANSI_ESC+ [ + <nombre de cols> + C;
+    protected static final String ANSI_INS = ANSI_ESC + "[2~";
+    protected static final String ANSI_RET = "\r";
+    protected static final String ANSI_DEL = ANSI_ESC + "[1P";
+    protected static final String ANSI_DEL_UNTIL_END = ANSI_ESC + "[K";
     
-    public static final int ESCAPE = 27;
-    public static final int RIGHT = 67;
-    public static final int LEFT = 68;
-    public static final int HOME = 72;
-    public static final int END = 70;
-    public static final int INS = 155;
-    public static final int RETURN = 13;
-    public static final int BCKS = 8;
-    public static final int DEL = 127;
+    protected static final int ESCAPE = 27;
+    protected static final int RIGHT = 67;
+    protected static final int LEFT = 68;
+    protected static final int HOME = 72;
+    protected static final int END = 70;
+    protected static final int INS = 155;
+    protected static final int RETURN = 13;
+    protected static final int BCKS = 8;
+    protected static final int DEL = 127;
     
 }
